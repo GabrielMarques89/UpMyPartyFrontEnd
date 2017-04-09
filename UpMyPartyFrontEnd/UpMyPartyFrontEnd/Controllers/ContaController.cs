9747ca5711bc601a13fda6bbc113bf7557d84c3e
GabrielMarques <gabriel.marques@geen.com.br>
2017-04-09 18:46:22 -0300

    Alterações iniciais

---
+++ UpMyPartyFrontEnd/UpMyPartyFrontEnd/Controllers/ContaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UpMyPartyFrontEnd.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return Cadastro();
+        }
+    }
+}
---
+++ UpMyPartyFrontEnd/UpMyPartyFrontEnd/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿#region Imports
+
+using System.Web.Mvc;
+
+#endregion
+
+namespace UpMyPartyFrontEnd.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
